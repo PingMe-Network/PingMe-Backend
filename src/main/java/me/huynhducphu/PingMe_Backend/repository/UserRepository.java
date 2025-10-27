f02d37bff15375d3120b767a662e9c4c2cc23d46
Le Tran Gia Huy <tranlehuygia2210@gmail.com>
2025-10-27 10:22:20 +0700

    Them 2 phuong thuc truy van cho connect va disconnect

--- src/main/java/me/huynhducphu/PingMe_Backend/repository/UserRepository.java
+++ src/main/java/me/huynhducphu/PingMe_Backend/repository/UserRepository.java
@@ -2,7 +2,11 @@ package me.huynhducphu.PingMe_Backend.repository;
 
 import me.huynhducphu.PingMe_Backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -15,5 +19,15 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
 
     Optional<User> getUserByEmail(String email);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE User u SET u.status = 'ONLINE' WHERE u.id = :userId")
+    void connect(@Param("userId") Long userId);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE User u SET u.status = 'OFFLINE' WHERE u.id = :userId")
+    void disconnect(@Param("userId") Long userId);
 
 }
