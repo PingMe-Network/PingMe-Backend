bac753131b8c48fc14110496d7666292d340ed3f
Le Tran Gia Huy <tranlehuygia2210@gmail.com>
2025-10-27 10:21:05 +0700

    Them phuong thuc getAllFriendshipsOfCurrentUser

--- src/main/java/me/huynhducphu/PingMe_Backend/service/friendship/FriendshipService.java
+++ src/main/java/me/huynhducphu/PingMe_Backend/service/friendship/FriendshipService.java
@@ -3,6 +3,9 @@ package me.huynhducphu.PingMe_Backend.service.friendship;
 import me.huynhducphu.PingMe_Backend.dto.request.friendship.FriendInvitationRequest;
 import me.huynhducphu.PingMe_Backend.dto.response.friendship.HistoryFriendshipResponse;
 import me.huynhducphu.PingMe_Backend.dto.response.friendship.UserFriendshipStatsResponse;
+import me.huynhducphu.PingMe_Backend.model.Friendship;
+
+import java.util.List;
 
 /**
  * Admin 8/19/2025
@@ -34,4 +37,6 @@ public interface FriendshipService {
     );
 
     UserFriendshipStatsResponse getUserFrendshipStats();
+
+    List<Friendship> getAllFriendshipsOfCurrentUser(String email);
 }
