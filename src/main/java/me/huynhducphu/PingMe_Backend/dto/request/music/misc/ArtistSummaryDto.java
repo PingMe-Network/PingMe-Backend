c6d6dadad6ec6357de6f2080c8b1a4b3d9e1b37e
Le Tran Gia Huy <tranlehuygia2210@gmail.com>
2025-11-20 22:11:17 +0700

    Thêm các DTO phụ cho việc gửi respone từ BE lên FE

---
+++ src/main/java/me/huynhducphu/PingMe_Backend/dto/request/music/misc/ArtistSummaryDto.java
@@ -0,0 +1,18 @@
+package me.huynhducphu.PingMe_Backend.dto.request.music.misc;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * @author Le Tran Gia Huy
+ * @created 20/11/2025 - 9:56 PM
+ * @project DHKTPM18ATT_Nhom10_PingMe_Backend
+ * @package me.huynhducphu.PingMe_Backend.dto.request.music.misc
+ */
+@Data
+@AllArgsConstructor
+public class ArtistSummaryDto {
+    private Long id;
+    private String name;
+    private String imgUrl; // Nếu cần hiện avatar nhỏ xíu
+}
