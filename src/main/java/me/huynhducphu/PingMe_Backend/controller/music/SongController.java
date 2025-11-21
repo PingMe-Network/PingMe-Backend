8e3aa823cdfa512dba76a7298829166c22030bac
NgocHung11 <22654091.hung@student.iuh.edu.vn>
2025-11-21 23:01:41 +0700

    thêm SongController

---
+++ src/main/java/me/huynhducphu/PingMe_Backend/controller/music/SongController.java
@@ -0,0 +1,30 @@
+package me.huynhducphu.PingMe_Backend.controller.music;
+
+import lombok.RequiredArgsConstructor;
+import me.huynhducphu.PingMe_Backend.dto.response.music.SongResponse;
+import me.huynhducphu.PingMe_Backend.service.music.SongService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/songs")
+@RequiredArgsConstructor
+public class SongController {
+
+    private final SongService songService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SongResponse> getSongDetail(@PathVariable Long id) {
+        SongResponse songResponse = songService.getSongById(id);
+        return ResponseEntity.ok(songResponse);
+    }
+
+    @GetMapping("/search/{title}")
+    public ResponseEntity<List<SongResponse>> getSongByTitle(@PathVariable String title) {
+        List<SongResponse> songResponses = songService.getSongByTitle(title);
+        return ResponseEntity.ok(songResponses);
+    }
+
+}
