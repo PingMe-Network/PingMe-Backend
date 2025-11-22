54cf429925eca9597c46322cd618c91101e3af30
Le Tran Gia Huy <tranlehuygia2210@gmail.com>
2025-11-22 14:30:52 +0700

    Chỉnh sửa lại GenreDto

--- src/main/java/me/huynhducphu/PingMe_Backend/dto/response/music/misc/GenreDto.java
+++ src/main/java/me/huynhducphu/PingMe_Backend/dto/response/music/misc/GenreDto.java
@@ -2,6 +2,7 @@ package me.huynhducphu.PingMe_Backend.dto.response.music.misc;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @author Le Tran Gia Huy
@@ -9,8 +10,10 @@ import lombok.Data;
  * @project DHKTPM18ATT_Nhom10_PingMe_Backend
  * @package me.huynhducphu.PingMe_Backend.dto.request.music.misc
  */
+
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class GenreDto {
     private Long id;
     private String name;
