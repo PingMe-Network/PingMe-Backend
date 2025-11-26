22979eb3a98a95a3ddc64f6fe769561de05337f3
Le Tran Gia Huy <tranlehuygia2210@gmail.com>
2025-11-26 18:11:00 +0700

    Bổ sung các Query cho album

--- src/main/java/me/huynhducphu/PingMe_Backend/repository/music/AlbumRepository.java
+++ src/main/java/me/huynhducphu/PingMe_Backend/repository/music/AlbumRepository.java
@@ -2,7 +2,11 @@ package me.huynhducphu.PingMe_Backend.repository.music;
 
 import me.huynhducphu.PingMe_Backend.model.music.Album;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 /**
 *@created 20/11/2025 - 6:21 PM
@@ -10,6 +14,13 @@ import org.springframework.stereotype.Repository;
 *@package me.huynhducphu.PingMe_Backend.repository
 *@author Le Tran Gia Huy
 */
+
 @Repository
-public interface AlbumRepository extends JpaRepository<Album, Integer> {
+public interface AlbumRepository extends JpaRepository<Album, Long> {
+
+    @Query(value = "SELECT * FROM albums WHERE id = :id AND is_deleted = true", nativeQuery = true)
+    Optional<Album> findSoftDeletedAlbum(@Param("id") Long id);
+
+    @Query(value = "SELECT * FROM albums WHERE id = :id", nativeQuery = true)
+    Optional<Album> findByIdIgnoringDeleted(@Param("id") Long id);
 }
