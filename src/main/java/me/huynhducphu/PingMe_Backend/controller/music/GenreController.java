bf182120ee0caadde1cacc33b597bf8a4d61fc87
Le Tran Gia Huy <tranlehuygia2210@gmail.com>
2025-11-26 18:08:53 +0700

    Bổ sung các API RESTful cho genre

--- src/main/java/me/huynhducphu/PingMe_Backend/controller/music/GenreController.java
+++ src/main/java/me/huynhducphu/PingMe_Backend/controller/music/GenreController.java
@@ -1,12 +1,13 @@
 package me.huynhducphu.PingMe_Backend.controller.music;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import me.huynhducphu.PingMe_Backend.dto.request.music.GenreRequest;
+import me.huynhducphu.PingMe_Backend.dto.response.music.GenreResponse;
 import me.huynhducphu.PingMe_Backend.dto.response.music.misc.GenreDto;
 import me.huynhducphu.PingMe_Backend.service.music.GenreService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Set;
@@ -25,7 +26,44 @@ public class GenreController {
     private final GenreService genreService;
 
     @GetMapping("/all")
-    public ResponseEntity<Set<GenreDto>> getAllGenres() {
+    public ResponseEntity<List<GenreResponse>> getAllGenres() {
         return ResponseEntity.ok(genreService.getAllGenres());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<GenreResponse> getGenreById(@PathVariable Long id){
+        return ResponseEntity.ok(genreService.getGenreById(id));
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<GenreResponse> saveGenre(
+            @Valid @RequestPart("genreRequest") GenreRequest genreRequest
+    ){
+        return ResponseEntity.ok(genreService.createGenre(genreRequest));
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<GenreResponse> updateGenre(
+            @PathVariable Long id, GenreRequest request
+    ) {
+        return ResponseEntity.ok(genreService.updateGenre(id, request));
+    }
+
+    @DeleteMapping("/soft-delete/{id}")
+    public ResponseEntity<Void> softDeleteGenre(@PathVariable Long id) {
+        genreService.softDeleteGenre(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/restore/{id}")
+    public ResponseEntity<Void> restoreGenre(@PathVariable Long id) {
+        genreService.restoreGenre(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/hard-delete/{id}")
+    public ResponseEntity<Void> hardDeleteGenre(@PathVariable Long id) {
+        genreService.hardDeleteGenre(id);
+        return ResponseEntity.ok().build();
     }
 }
