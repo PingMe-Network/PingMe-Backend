9b92909d910454a9424e63185e070d7f5a012a14
Le Tran Gia Huy <tranlehuygia2210@gmail.com>
2026-01-08 22:50:38 +0700

    Thành công xóa toàn bộ đoạn code liên quan đến chức năng quản lý chi tiêu ở BE

--- src/main/java/me/huynhducphu/PingMe_Backend/service/expense/ExpenseService.java
+++
@@ -1,8 +0,0 @@
-package me.huynhducphu.PingMe_Backend.service.expense;
-
-import me.huynhducphu.PingMe_Backend.dto.request.miniapp.expense.CreateTransactionRequest;
-import me.huynhducphu.PingMe_Backend.model.expense.ExpenseTransaction;
-
-public interface ExpenseService {
-    ExpenseTransaction createTransaction(CreateTransactionRequest request);
-}
