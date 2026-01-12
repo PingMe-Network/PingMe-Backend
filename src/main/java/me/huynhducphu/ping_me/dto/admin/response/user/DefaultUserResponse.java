ba66e966c7ed0fb92e486ced45d7f80ff8d3be17
phatdang <phatdang19052004@gmail.com>
2026-01-12 16:12:46 +0700

    feat(dto.admin.response.user.DefaultUserResponse): thêm các field để phục vụ chức năng xem thông tin chi tiết của tài khoản
    Thêm gender, address, dob, avatarUrl

--- src/main/java/me/huynhducphu/ping_me/dto/admin/response/user/DefaultUserResponse.java
+++ src/main/java/me/huynhducphu/ping_me/dto/admin/response/user/DefaultUserResponse.java
@@ -4,7 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import me.huynhducphu.ping_me.model.authorization.Role;
 import me.huynhducphu.ping_me.model.constant.AccountStatus;
+import me.huynhducphu.ping_me.model.constant.Gender;
+
+import java.time.LocalDate;
 
 /**
  * Admin 8/3/2025
@@ -17,5 +21,9 @@ public class DefaultUserResponse {
     Long id;
     String email;
     String name;
+    Gender gender;
+    String address;
+    LocalDate dob;
+    String avatarUrl;
     AccountStatus accountStatus;
 }
